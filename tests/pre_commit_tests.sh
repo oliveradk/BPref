969d5ed3d63d14947383dc1bce335cf279983727
oliveradk <danielskoch.oliver@gmail.com>
2022-06-23 12:31:46 -0700

    added pre-commit "smoke tests"

---
+++ tests/pre_commit_tests.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+. ./tests/run_PEBBLE_metaworld.sh
+if (($? == 0)) 
+then
+     echo "PEBBLE metaworld succeeded"
+else
+    echo "PEBBLE metaworld failed"
+    exit 1
+fi
+
+. ./tests/run_PEBBLE_dmc.sh
+if (($? == 0))
+then 
+    echo "PEBBLE dmc succeeded" 
+else
+    echo "PEBBLE dmc failed" 
+    exit 1
+fi 
+
+. ./tests/run_sac_metaworld.sh
+if (($? == 0))
+then 
+    echo "SAC metaworld succeeded" 
+else
+    echo "SAC metaworld failed" 
+    exit 1
+fi 
+
+. ./tests/run_sac_dmc.sh
+if (($? == 0))
+then 
+    echo "SAC dmc succeeded" 
+else
+    echo "SAC dmc failed" 
+    exit 1
+fi
